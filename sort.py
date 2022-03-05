8abfdca4bd6fe9236f18968cfed64c6872fdb2b7
JustinYSJSU <justin.yamamoto@sjsu.edu>
2022-03-05 15:14:06 -0800

    Added checks for empty list and non list parameter

--- sort.py
+++ sort.py
@@ -1,13 +1,18 @@
 def main():
-  myList = [3,2,4,89]
+  myList = [1,2,3,7]
   sort_List(myList)
   print(myList)
 
 #sort_list will take a list input and return it sorted
 def sort_List(myList):
+ if isinstance(myList, list) == False: #parameter is not a list
+  return None
+
  n = len(myList) #n is the length of the list
- if(n == 0): #list is empty
-  return none
+
+ if(n == 0): #list is empty  return None
+  return None
+
  i = 0
  while i < n:
   i2 = i + 1
