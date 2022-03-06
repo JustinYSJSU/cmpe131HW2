22e7fd56760428ddf17797851dc8d20af75af2d4
JustinYSJSU <justin.yamamoto@sjsu.edu>
2022-03-05 19:00:54 -0800

    edit double.py

--- double.py
+++ double.py
@@ -3,15 +3,9 @@
 def doubler(func):
  def wrapper():
   func()
- return wrapper
-
-def doubler2(func):
- def wrapper():
   func()
  return wrapper
 
-
-@doubler2
 @doubler
 def twoTime():
  print("This is part of a double call")
