92be9014bf90d63727b55f19cc56cdb34f715ad0
JustinYSJSU <justin.yamamoto@sjsu.edu>
2022-03-10 18:58:05 -0800

    added check for nonexistent file in doc analyzer

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,35 +1,42 @@
 
 def analyze(fileName):
+
  #get file that is going to be read
- myFile = open(fileName, "r")
- wordCounter = {} #dictionary to keep track of word count
+ #if that file does not exist, throw an exception
+ try:
+  myFile = open(fileName, "r")
+  wordCounter = {} #dictionary to keep track of word count
+  #get a list containing all the words
+  data = myFile.read()
+  words = data.split()
  
-#get a list containing all the words
- data = myFile.read()
- words = data.split()
- 
- #for each word in the file, check if its in the dictionary
- #if it is, addd to it's current count
- #if it's not, then add it to dictionary w/ a count of 1 
- for word in words:
-  if word == "i":
-   word = "I"
-  if word in wordCounter:
-   wordCounter[word]+=1
-  else:
-   wordCounter[word] = 1
+  #for each word in the file, check if its in the dictionary
+  #if it is, addd to it's current count
+  #if it's not, then add it to dictionary w/ a count of 1 
+  for word in words:
+   if word == "i":
+    word = "I"
+   if word in wordCounter:
+    wordCounter[word]+=1
+   else:
+    wordCounter[word] = 1
 
- sortedCounter = sorted(wordCounter.items(), key = lambda x:(-x[1], x[0]))
- sortedCounter = dict(sortedCounter)
+  sortedCounter = sorted(wordCounter.items(), key = lambda x:(-x[1], x[0]))
+  sortedCounter = dict(sortedCounter)
 
- print()
+  print()
 
- i = 0 
- for word, counter in sortedCounter.items():
-  print(f"{word}: {counter}", end = "\n")
-  i+=1
-  if(i == 5):
-   break
+  i = 0 
+  for word, counter in sortedCounter.items():
+   print(f"{word}: {counter}", end = "\n")
+   i+=1
+   if(i == 5):
+    break
+
+ except OSError as e:
+  print("FILE NOT FOUND")
+
+
 def main():
  analyze("document.txt")
 
