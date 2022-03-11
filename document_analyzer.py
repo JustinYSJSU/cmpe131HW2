5c63a9a4cc406e4ee3b3cca0490d9fc1bcd4a467
JustinYSJSU <justin.yamamoto@sjsu.edu>
2022-03-10 18:29:42 -0800

    more edits

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,3 +1,4 @@
+
 def analyze(fileName):
  #get file that is going to be read
  myFile = open(fileName, "r")
@@ -11,27 +12,25 @@ def analyze(fileName):
  #if it is, addd to it's current count
  #if it's not, then add it to dictionary w/ a count of 1 
  for word in words:
-  word = word.lower()
+  if word == "i":
+   word = "I"
   if word in wordCounter:
    wordCounter[word]+=1
   else:
    wordCounter[word] = 1
 
- #sort word count by largest count to smallest count
- #sort items by value in reverse
- #sorted() returns a list, use dict() to convert to dictionary
  sortedCounter = sorted(wordCounter.items(), key = lambda x:x[1], reverse = True)
  sortedCounter = dict(sortedCounter)
 
- #print the first 5 entries 
- i = 0
- for word, count in sortedCounter.items():
-  print(f"{word}: {count}", end = "\n")
+ print()
+
+ i = 0 
+ for word, counter in sortedCounter.items():
+  print(f"{word}: {counter}", end = "\n")
   i+=1
   if(i == 5):
    break
-  
-
 def main():
  analyze("document.txt")
+
 main()
